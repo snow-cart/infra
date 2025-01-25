4d1b73261bd58588dd363cfa2bff49febd6551db
mykole <160158910+snow-cart@users.noreply.github.com>
2025-01-25 15:46:32 +0200

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export PERSON_NAME="Kipras"
+export PERSON_NAME="Mykolas"
 # used to easily manage forked repos
-export GITHUB_USERNAME="kiprasmel"
+export GITHUB_USERNAME="snow-cart"
 export INFRA_REPO_URL="https://github.com/$GITHUB_USERNAME/infra"
 
